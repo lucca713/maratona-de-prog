51d96034b5ec01b119557c049ea58082e059e0e2
lucca713 <luccacerasomma03@gmail.com>
2024-05-21 23:24:38 -0300

    Ex 1037 iniciante beecrowd

---
+++ exercicios em c/Ex 1037.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    float n;
+    scanf("%f", &n);
+
+    if( n >= 0 && n <= 25){
+
+        printf("Intervalo [0,25]");
+
+    }else if (n >= 25 && n <= 50)
+    {
+        printf("Intervalo (25,50]");
+    }else if(n >= 50 && n <= 75){
+
+        printf("Intervalo (50,75]");
+
+    }else if(n >= 75 && n <= 100){
+
+        printf("Intervalo (75,100]");
+
+    }else{
+        printf("Fora de intervalo");
+    }
+    
+
+
+    
+    printf("\n");
+    return 0;
+}
